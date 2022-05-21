f147a48578177182adb5a8e6159e0f389e891119
PaulSong213 <songaliajohnpaul@gmail.com>
2022-05-21 18:12:43 +0800

    Add email html template

---
+++ TestingProject/Models/EmailSendModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnrollmentSystem.Models
+{
+    public class EmailSendModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [AllowHtml]
+        [Display(Name = "EmailString")]
+        public string EmailString { get; set; }
+
+    }
+}
